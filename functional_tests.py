49f2c861d97bc5a4bb1364e64ac2e94c5b23c960
Aranaris <vinceye.cm@gmail.com>
2017-06-02 03:05:22 -0400

    Refactored to use templates for home page

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class newVisitorTest(unittest.TestCase):
@@ -16,14 +17,29 @@ class newVisitorTest(unittest.TestCase):
 
         #The app should indicate that it is meant for creating to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
         #The user should start on entering a to-do item as the first thing on the site
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do item'
+                )
 
         #The user should be able to enter "Renew Driver's License" into a text box
+        inputbox.send_keys('Renew Driver\'s License')
 
         #Once the user hits enter, the page updates and displays the item in a list (i.e. "1: Renew Driver's License")
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Renew Driver\'s License' for row in rows)
+        )
 
         #The user still has a text box that allows them to add another item. The user wants to enter "Do Laundry"
+        self.fail('Finish the test!')
 
         #The page updates, and shows both items
 
