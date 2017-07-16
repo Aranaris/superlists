67731d1a74c60e3ac3064fccb666a907a7f325de
Aranaris <vinceye.cm@gmail.com>
2017-07-16 19:49:22 -0400

    set STATIC_ROOT in settings and added bootstrap stylings

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,4 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from contextlib import contextmanager
 from selenium.webdriver.common.keys import Keys
@@ -6,7 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import staleness_of
 import unittest
 
-class newVisitorTest(LiveServerTestCase):
+class newVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
